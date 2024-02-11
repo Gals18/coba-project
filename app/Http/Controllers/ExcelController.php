75d911ca96dca5be777c23117df4a8f354d3f7a7
Gals18 <galuhsaputrii18@gmail.com>
2024-02-11 12:25:16 +0700

    keenam

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
-use App\Exports\YourExcelExport;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\Exception;
 use App\Models\Berkas as ModelsBerkas;
 use App\Models\Pengguna;
 
@@ -14,11 +14,35 @@ class ExcelController extends Controller
     public function show($id)
     {
         $databerkas = ModelsBerkas::find($id);
-       
+       $datapegawai = Pengguna::find(session($id));
         $data = [
             'data'=>$databerkas,
+            'pegawai' => $datapegawai,
         ];
+        try {
+            $filePath = storage_path('app/pubic/fileExcel/{$data->file_excel}');
+            $reader = IOFactory::createReaderForFile($filePath);
+            $spreadsheet = $reader->load($filePath);
+            $sheet = $spreadsheet->getActiveSheet();
+
+            $data = [];
+            foreach ($sheet->getRowIterator() as $row) {
+                $rowData = [];
+                foreach ($row->getCellIterator() as $cell) {
+                    $rowData[] = $cell->getValue();
+                }
+                $data[] = $rowData;
+            }
+            // dd($data);
+            return view('berkas.detail', ['excelData' => $data]);
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
         //   dd(databerkas);
-        return view('berkas.detail', $data);
+    }
+
+    public function readExcelFromDirectory()
+    {
+       
     }
 }
--- resources/views/berkas/detail.blade.php
+++ resources/views/berkas/detail.blade.php
@@ -4,7 +4,9 @@
     <h5 class="card-title text-light fw-semibold mb-4">Data Berkas</h5>
     <p>{{ $data->id }}</p>
     <p>{{ $data->file_excel }}</p>
-
+    <p>{{ session('ktp') }}</p>
+    <p>{{ session('username') }}</p>
+    <img src="" alt="">
     <div id="excel-content"></div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.4/xlsx.full.min.js"></script>
@@ -13,14 +15,30 @@
         // $coba = ms-excel:ofe|u|Storage::disk('public')->url('fileBPJS/' . $data->file_excel);
         // console.log($coba);
     </script>
-    @php
+    {{-- @php
         $url = Storage::disk('public')->url('app/public/fileExcel/' . $data->file_excel);
         // dd($url);
-    @endphp
+    @endphp --}}
 
-    <iframe src="{{ $url }}" width="100%" height="600"></iframe>
 
-    {{-- <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR2aGJXxx_GgualpiDDHP08c_RvXg-DF77g26Q2yiQfSDI6GSNacmg3CQmmgd-2wQ/pubhtml?widget=true&amp;headers=false" width="100%" height="600"></iframe> --}}
+
+    <table border="1">
+
+        <tbody>
+            @foreach($excelData as $row)
+            <tr>
+                @foreach($row as $cell)
+                <td>{{ $cell }}</td>
+                @endforeach
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+{{-- 
+
+    <iframe src="{{ $url }}" width="100%" height="600"></iframe> --}}
+
+    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR2aGJXxx_GgualpiDDHP08c_RvXg-DF77g26Q2yiQfSDI6GSNacmg3CQmmgd-2wQ/pubhtml?widget=true&amp;headers=false" width="100%" height="600"></iframe>
     </section>
 
 @endsection
